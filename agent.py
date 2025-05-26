b3056d09497589cca66c3bacfb8cc4cf3acb77e3
Aswin Ajayan <aswinp2008@gmail.com>
2025-05-26 10:47:33 +0530

    tool calling made more reliable by system prompts and mcp roots

--- agent.py
+++ agent.py
@@ -6,7 +6,20 @@ fast = FastAgent("VibeShell - your CLI boiler plate generator")
 
 
 # Define the agent
-@fast.agent(name='greeter', instruction="You are a helpful AI Agent. You can use the mcp servers exposed to you to help the user",
+@fast.agent(name='greeter',
+            instruction="You are a helpful AI Agent."
+                        "<tool_calling>"
+                        "You have tools at your disposal to solve the coding task"
+                        "ALWAYS follow the tool call schema exactly as specified and"
+                        "make sure to provide all necessary parameters."
+                        "The conversation may reference tools that are no longer available."
+                        "NEVER call tools that are not explicitly provided."
+                        "you can call a tools multiple times help the users"
+                        "You can use the mcp servers exposed to you to help the user"
+                        "</tool_calling>"
+                        "use the mcp-roots to decode file paths"
+                        "append all file paths to /projects/workspace folder before using filesystem tool"
+                        "current working directory will be available at /projects/workspace",
             servers=["filesystem"])
 
 # Define the clients
